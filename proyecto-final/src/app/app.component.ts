4385d983e73aeffeadcff062d83e6ba2f156c23d
Caarlos645 <casanco2000@gmail.com>
2024-02-07 22:31:53 +0100

    commit

--- proyecto-final/src/app/app.component.ts
+++ proyecto-final/src/app/app.component.ts
@@ -1,13 +1,15 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { HeaderComponent } from './header/header.component';
+import { ListaProductosComponent } from './lista-productos/lista-productos.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
   imports: [
     RouterOutlet,
-    HeaderComponent
+    HeaderComponent,
+    ListaProductosComponent
   ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
